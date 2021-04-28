fd20640f99dd92e0b715781024f2d3e6a4e1d576
Wemyss <sam.w@canva.com>
2021-04-28 16:39:35 +1000

    .

--- composeRefs.ts
+++ composeRefs.ts
@@ -2,36 +2,31 @@ import {MutableRefObject, Ref} from 'react'
 
 type OptionalRef<T> = Ref<T> | undefined
 
-export default function composeRefs<T>(...refs: [OptionalRef<T>, OptionalRef<T>, ...Array<OptionalRef<T>>]): Ref<T> {
-  if (refs.length === 2) { // micro-optimize the hot path
-    return composeTwoRefs(refs[0], refs[1]) || null
+export default function composeRefs<T>(...refs: OptionalRef<T>[]): Ref<T> {
+  if (refs.length <= 2) { // micro-optimize the hot path
+    return composeTwoRefs(refs[0], refs[1])
   }
-
-  return refs.reduce<OptionalRef<T>>(composeTwoRefs, null)
+  return refs.reduce<Ref<T>>(composeTwoRefs, null)
 }
 
 type NonNullRef<T> = NonNullable<Ref<T>>
 const composedRefCache = new WeakMap<NonNullRef<unknown>, WeakMap<NonNullRef<unknown>, NonNullRef<unknown>>>()
 
-function composeTwoRefs<T>(ref1: OptionalRef<T>, ref2: OptionalRef<T>): OptionalRef<T> {
-  if (ref1 && ref2) {
-    const ref1Cache = composedRefCache.get(ref1) || new WeakMap<NonNullRef<unknown>, NonNullRef<unknown>>()
-    composedRefCache.set(ref1, ref1Cache)
-
-    const composedRef = ref1Cache.get(ref2) || ((instance: T): void => {
-      updateRef(ref1, instance)
-      updateRef(ref2, instance)
-    })
-    ref1Cache.set(ref2, composedRef)
-
-    return composedRef as NonNullRef<T>
+function composeTwoRefs<T>(ref1: OptionalRef<T>, ref2: OptionalRef<T>): Ref<T> {
+  if (!ref1 || !ref2) {
+    return ref1 || ref2 || null
   }
 
-  if (!ref1) {
-    return ref2
-  } else {
-    return ref1
-  }
+  const ref1Cache = composedRefCache.get(ref1) || new WeakMap<NonNullRef<unknown>, NonNullRef<unknown>>()
+  composedRefCache.set(ref1, ref1Cache)
+
+  const composedRef = ref1Cache.get(ref2) || ((instance: T): void => {
+    updateRef(ref1, instance)
+    updateRef(ref2, instance)
+  })
+  ref1Cache.set(ref2, composedRef)
+
+  return composedRef as NonNullRef<T>
 }
 
 function updateRef<T>(ref: NonNullRef<T>, instance: null | T): void {
